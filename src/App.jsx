e103f3d3c40655cf77d8657a5b3a5c39e493d63f
macbook <macbook@macbooks-mbp.myfiosgateway.com>
2024-06-06 22:04:07 -0400

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,41 @@
-      <div>
-       
+import { useState } from 'react'
+import './App.css'
+import {puppyList} from './data.js'
+
+
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log("puppyList",puppyList)
+  const [featPupId, setFeatPupId] = useState(null) 
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+
+  return (
+      <div className="App">
+        {
+          puppies.map((puppy) => {
+            return <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p> //adding key when returning
+          })
+        }
+      <div className="Puppies"> {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}</div>
       </div>
-     
+  )
+}
+
+
+export default App
+
+
+
+
+
 
 
